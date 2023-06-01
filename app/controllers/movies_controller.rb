2fc5895152bef5891495aebfb823c1a522939dba
Christian Bongard <christian.bongard90@gmail.com>
2023-06-01 11:36:01 +0400

    added case insensitive search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     # If a parameter is present, filter the movies using the said parameter
     if params[:query].present?
-      @movies = @movies.where(title: params[:query])
+      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
     end
   end
 end
