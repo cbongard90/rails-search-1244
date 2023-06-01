8320da53d12ad9ce86154b1d6299047adc3661f9
Christian Bongard <christian.bongard90@gmail.com>
2023-06-01 12:22:48 +0400

    added tv shows for pg search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,16 @@
 class Movie < ApplicationRecord
   include PgSearch::Model
+  searchkick
   belongs_to :director
 
-  pg_search_scope :global_search,
-    against: [:title, :synopsis],
-    associated_against: {
-      director: [ :first_name, :last_name ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
+  multisearchable against: [:title, :synopsis]
+
+  # pg_search_scope :global_search,
+  #   against: [:title, :synopsis],
+  #   associated_against: {
+  #     director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 end
