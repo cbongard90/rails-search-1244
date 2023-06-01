9fbd0203b2fd6467faa7f9064f76ea4e4990fdb0
Christian Bongard <christian.bongard90@gmail.com>
2023-06-01 11:48:51 +0400

    added search for director last name and first name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,14 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     # If a parameter is present, filter the movies using the said parameter
     if params[:query].present?
-      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
-      @movies = @movies.where(sql_query, query: "%#{params[:query]}%")
+      # sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      sql_query = <<~SQL
+        movies.title ILIKE :query
+        OR movies.synopsis ILIKE :query
+        OR directors.first_name ILIKE :query
+        OR directors.last_name ILIKE :query
+      SQL
+      @movies = @movies.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     end
   end
 end
