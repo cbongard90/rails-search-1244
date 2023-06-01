7b055bd1c025e0afcaf19d147ef5c2dddb9584f2
Christian Bongard <christian.bongard90@gmail.com>
2023-06-01 12:01:51 +0400

    added pg search for title and synopsis

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ApplicationRecord
+  include PgSearch::Model
   belongs_to :director
+
+  pg_search_scope :search_by_title_and_synopsis,
+    against: [:title, :synopsis],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
